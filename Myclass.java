52b132795808b779e676edfe46525737a75d967c
im-exol <joshnachowdarygudiputi4@gmail.com>
2021-11-23 11:25:34 +0530

    committed

---
+++ Myclass.java
@@ -0,0 +1,9 @@
+class Myclass
+{
+public static void main( String args[] )
+{
+System.out.println("EXO WE ARE ONE");
+System.out.println("saranghaja!");
+System.out.println("SUHO,XIUMIN,LAY,BAEKHYUN,CHEN,CHANYEOL,DO,KAI,SEHUN");
+}
+}
